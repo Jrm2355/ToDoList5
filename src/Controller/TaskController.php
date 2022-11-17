a1bbe80d97ace6d001e966b2e6b8290308e4eba8
Jeremyon <jeremyon.jy@gmail.com>
2022-11-17 10:31:09 +0100

    symfony insight

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -24,7 +24,6 @@ class TaskController extends AbstractController
         ]);
     }
 
-    #[Route('/tasks/create', name: 'task_create')]
     /**
      * @Route("/task/create", name="task_create")
      */
